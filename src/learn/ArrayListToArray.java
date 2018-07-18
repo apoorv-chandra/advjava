0036d0e556b56847a664e4e460c284b5bbe2644b
apoorvchandra <apoorvchandra@yahoo.com>
2018-07-18 15:52:30 +0530

    arraylist to string array

---
+++ src/learn/ArrayListToArray.java
@@ -0,0 +1,22 @@
+package learn;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ArrayListToArray {
+	 public static void main(String[] args) {
+	        List<String> list = new ArrayList<String>();
+	        
+	        list.add("array");
+	        list.add("arraylist");
+	        list.add("convertion");
+	        list.add("javaprogram");
+	        
+	        String [] str = list.toArray(new String[list.size()]);
+	        
+	        for (int j = 0; j < str.length; j++) {
+	        	  System.out.println(str[j]);			}  
+	    } 
+	
+}

a284a7dada04ef81f937eab2f9112a5e6443962a
apoorvchandra <apoorvchandra@yahoo.com>
2018-07-18 16:59:42 +0530

    frequency of string array

--- src/learn/ArrayListToArray.java
+++ src/learn/ArrayListToArray.java
@@ -1,8 +1,6 @@
 package learn;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class ArrayListToArray {
 	 public static void main(String[] args) {

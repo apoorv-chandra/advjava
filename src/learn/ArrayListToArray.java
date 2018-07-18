9122ae4bef94db6658264cbcefcd69bed878d3bd
apoorvchandra <apoorvchandra@yahoo.com>
2018-07-18 16:59:54 +0530

    changes

--- src/learn/ArrayListToArray.java
+++ src/learn/ArrayListToArray.java
@@ -1,9 +1,14 @@
 package learn;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ArrayListToArray {
 	 public static void main(String[] args) {
+		 String[] str = {"abc", "dcs", "Abg","abc","hdf"};
 	        List<String> list = new ArrayList<String>();
 	        
 	        list.add("array");
@@ -11,10 +16,23 @@ public class ArrayListToArray {
 	        list.add("convertion");
 	        list.add("javaprogram");
 	        
-	        String [] str = list.toArray(new String[list.size()]);
+	        String [] str1 = list.toArray(new String[list.size()]);
 	        
-	        for (int j = 0; j < str.length; j++) {
-	        	  System.out.println(str[j]);			}  
-	    } 
+	        for (int j = 0; j < str1.length; j++) 
+	        {
+	        	  System.out.println(str1[j]);			
+	        }
+	      Map<String, Integer> stringtohashmap = new LinkedHashMap<String, Integer>();
+	      int i=0;
+	    		  for (String string : str) 
+	    		  {
+	    			  stringtohashmap.put(string, i++);
+			    		}
+	    		  System.out.println(stringtohashmap);
+	    		  //Counting frequency of words in string array
+	    		  Map<Object, Long> freqCaseIn = Arrays.stream(str)
+	    				  .collect(Collectors.groupingBy(String::toLowerCase, Collectors.counting()));
+	    		  System.out.println(freqCaseIn);
+	 } 
 	
 }

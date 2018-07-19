4a2b016a498153e3c8029bd9d31f80c66d40741d
apoorvchandra <apoorvchandra@yahoo.com>
2018-07-19 19:30:23 +0530

    libraries added

---
+++ src/learn/ClickAll.java
@@ -0,0 +1,47 @@
+package learn;
+
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ClickAll {
+	public static void main(String[] args) 
+	{
+		System.setProperty("webdriver.chrome.driver", 
+				".\\exefiles\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://helloparent.in/");
+	try {
+		Thread.sleep(5000);
+		System.out.println("Waiting for loading webpage");
+	} catch (InterruptedException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	}
+	List<WebElement> allelements = driver.findElements(By.tagName("a"));
+	    for(int i=0;i<allelements.size();i++)
+		 {
+		allelements.get(i).click();
+		System.out.println("CLicked on->" + allelements.get(i));
+		//break;
+		/*try {
+			Thread.sleep(10000);
+			System.out.println("clicked and waiting for load");
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+		//driver.navigate().back();
+		/*try {
+			Thread.sleep(3000);
+			System.out.println("clicked for back");
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+	}
+}
+}
